dc24f038f82d0744758046e9f68b7c9d4a8863e3
jotis.nikolis@gmail.com <jotis.nikolis@gmail.com>
2025-05-30 14:59:53 +0200

    test3

--- uppg6.js
+++ uppg6.js
@@ -5,7 +5,7 @@ function uppg6(){
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
 
- const names = ["Jotis", "Linnea", "Anna", "Tim"]
+ let names = ["Jotis", "Linnea", "Anna", "Tim"]
 
 for (let i = 0; i < names.length; i++) {
     console.log(names[i])
