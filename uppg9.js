5400e119cead72cfc5f9b4283f79ac2f19844c8e
jotis.nikolis@gmail.com <jotis.nikolis@gmail.com>
2025-05-30 15:45:09 +0200

    test13

--- uppg9.js
+++ uppg9.js
@@ -1,6 +1,4 @@
-
-
-function uppg9 (){
+function uppg9 () {
     // skapa en funktion som heter sort och tar in en array av siffror som argument
     // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
     // i if-sattsen skriv ut i konsolen resultatet
@@ -8,22 +6,19 @@ function uppg9 (){
 
     // anropa funktionen och skicka med en array som argument
 
-const number = [3, 15, 20, 5, 79, 78, 51, 32, 5001];
-
+    const number = [3, 15, 20, 5, 79, 78, 51, 32, 5001];
 
     sort(number);
 
     function sort(arr) {
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] % 2 === 0) {
-                console.log(`${arr[i]} jämt`);
+                console.log(arr[i], "jämt");
             } else {
-                console.log(`${arr[i]} udda`);
+                console.log(arr[i], "udda");
             }
         }
     }
 }
-
-
 
 module.exports = { uppg9 };
